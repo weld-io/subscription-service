c7104ec43f98fffd0b4b415bd283702b24167369
Tom Söderlund <tom@tomsoderlund.com>
2018-02-28 13:12:46 +0100

    Disable JWT - POST User

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -13,8 +13,13 @@ module.exports = function (app, config) {
 	if (process.env.DISABLE_JWT !== 'true') {
 		app.use(
 			jwt({ secret: process.env.JWT_SECRET })
+				// When not to use JWT:
 				.unless({ path: [
+					// Everything with Plans
 					{ url: /\/api\/plans/i, methods: ['GET'] },
+					// Create new User
+					{ url: '/api/users', methods: ['POST'] },
+					// Stripe renewal
 					'/api/subscriptions/renew',
 				] })
 		);
