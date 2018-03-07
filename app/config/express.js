6ada42a504bde9113a6f4a65ce4872cb57ecd803
Tom Söderlund <tom@tomsoderlund.com>
2018-03-07 11:15:49 +0100

    Error handling

--- app/config/express.js
+++ app/config/express.js
@@ -27,29 +27,19 @@ module.exports = function (app, config) {
 	// Require in all API controllers
 	glob.sync(config.root + '/controllers/api/*.js').forEach(controllerPath => require(controllerPath)(app, config));
 
-	// If no matching route found -> 404
+	// Error: If no matching route found -> 404
 	app.use(function (req, res, next) {
 		var err = new Error('Not Found');
 		err.status = 404;
 		next(err);
 	});
 
-	if (app.get('env') === 'development') {
-		app.use(function (err, req, res, next) {
-			res.status(err.status || 500);
-			res.json({
-				message: err.message,
-				error: err,
-				title: 'error'
-			});
-		});
-	}
-
+	// Error: others
 	app.use(function (err, req, res, next) {
 		res.status(err.status || 500);
 		res.json({
 			message: err.message,
-			error: {},
+			error: err,
 			title: 'error'
 		});
 	});
