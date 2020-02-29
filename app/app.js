89ac93dfd24515e361f821b6bbb6971a31ef361e
Tom Söderlund <tom@tomsoderlund.com>
2020-02-29 09:41:47 +0100

    config/database

--- app/app.js
+++ app/app.js
@@ -1,24 +1,10 @@
 const express = require('express')
-const glob = require('glob')
-const mongoose = require('mongoose')
 
 const config = require('./config/config')
-
-mongoose.Promise = Promise
-console.log(`Connecting to database: ${config.db}`)
-mongoose.connect(config.db, { useMongoClient: true })
-var db = mongoose.connection
-db.on('error', function () {
-  throw new Error('unable to connect to database at ' + config.db)
-})
-
-// Require in all models
-glob.sync(config.root + '/models/*.js').forEach(require)
+const { closeDatabase } = require('./config/database')
 
 var app = express()
 require('./config/express')(app, config)
 
 module.exports = app
-module.exports.closeDatabase = function () {
-  mongoose.connection.close()
-}
+module.exports.closeDatabase = closeDatabase
