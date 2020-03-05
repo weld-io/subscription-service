daafc4a1a8d8c4ae7e09c8931820eb1c1bdb7e1f
Tom Söderlund <tom@tomsoderlund.com>
2020-03-05 19:34:45 +0100

    Timestamp on server startup

--- app/server.js
+++ app/server.js
@@ -4,5 +4,5 @@ const app = require('./app')
 const config = require('./config/config')
 
 const portNumber = process.env.PORT || config.port
-console.log('subscription-service running on http://localhost:' + portNumber)
+console.log(`[${new Date().getHours()}:${new Date().getMinutes()}] subscription-service running on http://localhost:${portNumber}`)
 app.listen(portNumber)
